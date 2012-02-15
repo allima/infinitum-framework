f044bd54e3cfde0e4374688b372677864582012f
ttreat31 <ttreat31@gmail.com>
2012-02-15 02:31:55 +0000

    Updated test project to test dynamic creation of tables.

--- InfinitumTest/src/com/clarionmedia/infinitumtest/InfinitumTestActivity.java
+++ InfinitumTest/src/com/clarionmedia/infinitumtest/InfinitumTestActivity.java
@@ -1,32 +1,34 @@
 package com.clarionmedia.infinitumtest;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.os.Bundle;
 
+import com.clarionmedia.infinitum.context.ApplicationContext;
 import com.clarionmedia.infinitum.context.ApplicationContextFactory;
 import com.clarionmedia.infinitum.context.exception.InfinitumConfigurationException;
-import com.clarionmedia.infinitum.orm.persistence.ObjectMapper;
+import com.clarionmedia.infinitum.orm.sqlite.SqliteTemplate;
 import com.clarionmedia.infinitumtest.domain.TestModel;
 
 public class InfinitumTestActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        try {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		ApplicationContext appCtx = null;
+		try {
 			ApplicationContextFactory.configure(this, R.xml.infinitum);
-			TestModel test = new TestModel();
-			test.setId(99);
-			test.setMyField("hello world!");
-			test.setX(42);
-			ContentValues cv = ObjectMapper.mapModel(test);
-			cv.describeContents();
+			appCtx = ApplicationContextFactory.getApplicationContext();
 		} catch (InfinitumConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+
+		TestModel test = new TestModel();
+		SqliteTemplate sqlite = new SqliteTemplate(this, appCtx);
+		sqlite.open();
+		sqlite.save(test);
+		sqlite.close();
+	}
 }
