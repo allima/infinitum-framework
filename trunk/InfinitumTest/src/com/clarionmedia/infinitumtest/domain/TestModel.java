e3fb3b8fdf83a4be3727ff7cfbe86847a041101e
ttreat31@gmail.com <ttreat31@gmail.com@c94df8b1-cb47-dd85-f3ca-b5c9742c5caf>
2012-02-15 02:31:55 +0000

    Updated test project to test dynamic creation of tables.

--- trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/domain/TestModel.java
+++ trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/domain/TestModel.java
@@ -1,26 +1,40 @@
 package com.clarionmedia.infinitumtest.domain;
 
-import com.clarionmedia.infinitum.orm.annotation.Column;
-import com.clarionmedia.infinitum.orm.annotation.Entity;
-import com.clarionmedia.infinitum.orm.annotation.PrimaryKey;
-import com.clarionmedia.infinitum.orm.annotation.Table;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
-@Entity
+import com.clarionmedia.infinitum.orm.annotation.Column;
+import com.clarionmedia.infinitum.orm.annotation.Table;
+import com.clarionmedia.infinitum.orm.annotation.Unique;
+
 @Table(name = "test")
 public class TestModel {
-	
-	@PrimaryKey
+
 	private long mId;
 
 	@Column(name = "field")
 	private String mMyField;
-	
-	private int mX;
-	
+
+	private int mFoo;
+
+	private Date mBar;
+
+	@Unique
+	private double mBam;
+
+	public TestModel() {
+		Random rand = new Random(Calendar.getInstance().getTimeInMillis());
+		mBar = new Date();
+		mFoo = rand.nextInt();
+		mMyField = "hello world!";
+		setBam(rand.nextDouble());
+	}
+
 	public long getId() {
 		return mId;
 	}
-	
+
 	public void setId(long id) {
 		mId = id;
 	}
@@ -33,12 +47,28 @@ public class TestModel {
 		mMyField = myField;
 	}
 
-	public int getX() {
-		return mX;
+	public int getFoo() {
+		return mFoo;
 	}
 
-	public void setX(int x) {
-		mX = x;
+	public void setFoo(int foo) {
+		mFoo = foo;
+	}
+
+	public Date getBar() {
+		return mBar;
+	}
+
+	public void setBar(Date bar) {
+		mBar = bar;
+	}
+
+	public double getBam() {
+		return mBam;
+	}
+
+	public void setBam(double bam) {
+		mBam = bam;
 	}
 
 }
