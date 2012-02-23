371d499d2eeddb2295ad57779ef535aef09ed9ad
ttreat31@gmail.com <ttreat31@gmail.com@c94df8b1-cb47-dd85-f3ca-b5c9742c5caf>
2012-02-23 07:02:44 +0000

    Reflected annotation changes.

--- trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/SqliteTemplateActivity.java
+++ trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/SqliteTemplateActivity.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2012 Tyler Treat
+ * 
+ * This file is part of Infinitum Framework.
+ *
+ * Infinitum Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Infinitum Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Infinitum Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.clarionmedia.infinitumtest;
 
 import java.lang.reflect.Field;
@@ -5,11 +24,13 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import com.clarionmedia.infinitum.context.ApplicationContextFactory;
-import com.clarionmedia.infinitum.orm.criteria.Criteria;
-import com.clarionmedia.infinitum.orm.criteria.criterion.BinaryExpression;
+import com.clarionmedia.infinitum.orm.ManyToManyRelationship;
+import com.clarionmedia.infinitum.orm.criteria.GenCriteria;
 import com.clarionmedia.infinitum.orm.criteria.criterion.Conditions;
-import com.clarionmedia.infinitum.orm.criteria.criterion.Criterion;
+import com.clarionmedia.infinitum.orm.persistence.PersistenceResolution;
+import com.clarionmedia.infinitum.orm.sql.SqlBuilder;
 import com.clarionmedia.infinitum.orm.sqlite.SqliteTemplate;
+import com.clarionmedia.infinitumtest.domain.Foo;
 import com.clarionmedia.infinitumtest.domain.TestModel;
 
 public class SqliteTemplateActivity extends Activity {
@@ -26,10 +47,7 @@ public class SqliteTemplateActivity extends Activity {
 		sqlite.open();
 		// sqlite.save(model);
 		// TestModel loaded = sqlite.load(TestModel.class, 2L);
-		Criteria<TestModel> criteria = sqlite.createCriteria(TestModel.class);
-		criteria.add(Conditions.eq("mMyField", "hello world!"));
-		String sql = criteria.toSql();
-		List<TestModel> results = criteria.toList();
+		TestModel loaded = sqlite.load(TestModel.class, 42L);
 		sqlite.close();
 	}
 
