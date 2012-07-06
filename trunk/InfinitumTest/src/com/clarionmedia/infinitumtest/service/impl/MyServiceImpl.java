fa5a19ba192cd2a5696b5b803e483a384cc69e6a
ttreat31@gmail.com <ttreat31@gmail.com@c94df8b1-cb47-dd85-f3ca-b5c9742c5caf>
2012-07-06 01:50:31 +0000

    Updated test project to test bean autowiring.

--- trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/service/impl/MyServiceImpl.java
+++ trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/service/impl/MyServiceImpl.java
@@ -1,10 +1,16 @@
 package com.clarionmedia.infinitumtest.service.impl;
 
+import com.clarionmedia.infinitum.di.annotation.Autowired;
 import com.clarionmedia.infinitumtest.dao.MyDao;
 import com.clarionmedia.infinitumtest.service.MyService;
 
 public class MyServiceImpl implements MyService {
 	
 	private MyDao mDao;
+	
+	@Autowired
+	public MyServiceImpl(MyDao dao) {
+		mDao = dao;
+	}
 
 }
