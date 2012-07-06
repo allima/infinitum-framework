63ff8dd2c0c869059600f6195a320326b6a335f2
ttreat31 <ttreat31@gmail.com>
2012-07-06 21:28:25 +0000

    Began implementing generic RestfulClient. PUT methods not yet implemented. Also need to address configuration for generic client, i.e. should it store a host?

--- InfinitumFramework/src/com/clarionmedia/infinitum/rest/RestfulClient.java
+++ InfinitumFramework/src/com/clarionmedia/infinitum/rest/RestfulClient.java
@@ -19,6 +19,26 @@
 
 package com.clarionmedia.infinitum.rest;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
@@ -36,7 +56,7 @@ import com.clarionmedia.infinitum.logging.Logger;
  * @version 1.0
  * @since 07/04/12
  */
-public abstract class RestfulClient {
+public abstract class RestfulClient implements WebServiceClient {
 
 	protected InfinitumContext mContext;
 	protected String mHost;
@@ -60,8 +80,91 @@ public abstract class RestfulClient {
 	public final void prepare() {
 		mContext = ContextFactory.getInstance().getContext();
 		mHost = mContext.getRestfulConfiguration().getRestHost();
-		mIsAuthenticated = mContext.getRestfulConfiguration().isRestAuthenticated();
+		mIsAuthenticated = mContext.getRestfulConfiguration()
+				.isRestAuthenticated();
 		mAuthStrategy = mContext.getRestfulConfiguration().getAuthStrategy();
+	}
+
+	@Override
+	public RestResponse executeGet(String uri) {
+		return executeRequest(new HttpGet(uri));
+	}
+
+	@Override
+	public RestResponse executeGet(String uri, Map<String, String> headers) {
+		HttpGet httpGet = new HttpGet(uri);
+		for (Entry<String, String> header : headers.entrySet()) {
+			httpGet.addHeader(header.getKey(), header.getValue());
+		}
+		return executeRequest(httpGet);
+	}
+
+	@Override
+	public RestResponse executePost(String uri, String postBody,
+			String contentType) {
+		HttpPost httpPost = new HttpPost(uri);
+		httpPost.addHeader("content-type", contentType);
+		try {
+			httpPost.setEntity(new StringEntity(postBody, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			mLogger.error(
+					"Unable to send POST request (could not encode message body)",
+					e);
+			return null;
+		}
+		return executeRequest(httpPost);
+	}
+	
+	@Override
+	public RestResponse executePost(String uri, String postBody,
+			String contentType, Map<String, String> headers) {
+		HttpPost httpPost = new HttpPost(uri);
+		for (Entry<String, String> header : headers.entrySet()) {
+			httpPost.addHeader(header.getKey(), header.getValue());
+		}
+		httpPost.addHeader("content-type", contentType);
+		try {
+			httpPost.setEntity(new StringEntity(postBody, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			mLogger.error(
+					"Unable to send POST request (could not encode message body)",
+					e);
+			return null;
+		}
+		return executeRequest(httpPost);
+	}
+	
+	@Override
+	public RestResponse executePost(String uri, InputStream postBody, int postBodyLength, String contentType) {
+		HttpPost httpPost = new HttpPost(uri);
+		httpPost.addHeader("content-type", contentType);
+		httpPost.setEntity(new InputStreamEntity(postBody, postBodyLength));
+		return executeRequest(httpPost);
+	}
+	
+	@Override
+	public RestResponse executePost(String uri, InputStream postBody, int postBodyLength, String contentType, Map<String, String> headers) {
+		HttpPost httpPost = new HttpPost(uri);
+		for (Entry<String, String> header : headers.entrySet()) {
+			httpPost.addHeader(header.getKey(), header.getValue());
+		}
+		httpPost.addHeader("content-type", contentType);
+		httpPost.setEntity(new InputStreamEntity(postBody, postBodyLength));
+		return executeRequest(httpPost);
+	}
+	
+	@Override
+	public RestResponse executeDelete(String uri) {
+		return executeRequest(new HttpDelete(uri));
+	}
+	
+	@Override
+	public RestResponse executeDelete(String uri, Map<String, String> headers) {
+		HttpDelete httpDelete = new HttpDelete(uri);
+		for (Entry<String, String> header : headers.entrySet()) {
+			httpDelete.addHeader(header.getKey(), header.getValue());
+		}
+		return executeRequest(httpDelete);
 	}
 
 	/**
@@ -72,11 +175,45 @@ public abstract class RestfulClient {
 	 */
 	protected HttpParams getHttpParams() {
 		HttpParams httpParams = new BasicHttpParams();
-		HttpConnectionParams
-				.setConnectionTimeout(httpParams, mContext.getRestfulConfiguration().getConnectionTimeout());
-		HttpConnectionParams.setSoTimeout(httpParams, mContext.getRestfulConfiguration().getResponseTimeout());
+		HttpConnectionParams.setConnectionTimeout(httpParams, mContext
+				.getRestfulConfiguration().getConnectionTimeout());
+		HttpConnectionParams.setSoTimeout(httpParams, mContext
+				.getRestfulConfiguration().getResponseTimeout());
 		HttpConnectionParams.setTcpNoDelay(httpParams, true);
 		return httpParams;
+	}
+
+	private RestResponse executeRequest(HttpUriRequest httpRequest) {
+		mLogger.debug("Sending " + httpRequest.getMethod() + " request to "
+				+ httpRequest.getURI() + " with "
+				+ httpRequest.getAllHeaders().length + " headers");
+		RestResponse restResponse = new RestResponse();
+		HttpClient httpClient = new DefaultHttpClient(getHttpParams());
+		try {
+			HttpResponse response = httpClient.execute(httpRequest);
+			StatusLine statusLine = response.getStatusLine();
+			restResponse.setStatusCode(statusLine.getStatusCode());
+			for (Header header : response.getAllHeaders()) {
+				restResponse.addHeader(header.getName(), header.getValue());
+			}
+			HttpEntity entity = response.getEntity();
+			if (entity == null) {
+				restResponse.setResponseData(new byte[]{});
+			} else {
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				entity.writeTo(out);
+				out.close();
+				restResponse.setResponseData(out.toByteArray());
+			}
+			return restResponse;
+		} catch (ClientProtocolException e) {
+			mLogger.error("Unable to send " + httpRequest.getMethod()
+					+ " request", e);
+			return null;
+		} catch (IOException e) {
+			mLogger.error("Unable to read web service response", e);
+			return null;
+		}
 	}
 
 }
