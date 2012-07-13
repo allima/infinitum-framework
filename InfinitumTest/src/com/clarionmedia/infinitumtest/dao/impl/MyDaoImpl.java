99e3b54ffd508f429080b10807538d113efd8108
ttreat31 <ttreat31@gmail.com>
2012-07-13 05:23:49 +0000

    Added some AOP testing code.

--- InfinitumTest/src/com/clarionmedia/infinitumtest/dao/impl/MyDaoImpl.java
+++ InfinitumTest/src/com/clarionmedia/infinitumtest/dao/impl/MyDaoImpl.java
@@ -1,7 +1,9 @@
 package com.clarionmedia.infinitumtest.dao.impl;
 
+import com.clarionmedia.infinitum.di.annotation.Bean;
 import com.clarionmedia.infinitumtest.dao.MyDao;
 
+@Bean("myDao")
 public class MyDaoImpl implements MyDao {
 
 }
