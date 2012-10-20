8d9f351497a93a882abcf95b71ca48bfa7db6b54
ttreat31@gmail.com <ttreat31@gmail.com@c94df8b1-cb47-dd85-f3ca-b5c9742c5caf>
2012-10-20 03:30:50 +0000

    Updated test project.

--- trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/domain/SideA.java
+++ trunk/InfinitumTest/src/com/clarionmedia/infinitumtest/domain/SideA.java
@@ -6,7 +6,7 @@ public class SideA extends AbstractBase {
 	
 	private String mName = "hello";
 	
-	@OneToOne(className = "com.clarionmedia.infinitumtest.domain.SideB", column = "side_b", name = "A-B")
+	@OneToOne(className = "com.clarionmedia.infinitumtest.domain.SideB", column = "side_b", name = "A-B", owner = SideA.class)
 	private SideB mSideB;
 
 	public void setSideB(SideB mSideB) {
